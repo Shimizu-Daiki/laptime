b5f02caf7458f9835ee69410d0eb73064d3d1f6b
Ubuntu <ubuntu@ip-172-31-8-140.ap-northeast-1.compute.internal>
2020-07-10 08:10:51 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,12 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand font-weight-bold" href="/" style="font-size : 24px">lap-time</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+       
+    </nav>
+</header>
